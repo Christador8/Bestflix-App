920f5656daa07e4462c317ed0c731f6cd718e186
Christa Dor <christydove8@gmail.com>
2025-09-07 14:00:50 -0400

    Remove automatic login redirect - show home page by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Player from './pages/Player/Player';
 import Signup from './pages/Signup.jsx';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebase';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 function AppRoutes() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("User is signed in:", user);
-        navigate('/');
-      } else {
-        console.log("No user is signed in.");
-        navigate('/login');
-      }
-    });
-
-    return () => unsubscribe();
-  }, [navigate]);
-
   return (
     <>
       <Navbar />
